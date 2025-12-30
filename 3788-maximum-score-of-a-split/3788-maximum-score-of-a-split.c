0c975a681df4f09d9f9567f2acdf477faed6296e
chagit levi <chagit3266@gmail.com>
2025-12-30 14:57:29 +0200

    Time: 4 ms (71.16%), Space: 20.3 MB (75.12%) - LeetHub

--- 3788-maximum-score-of-a-split/3788-maximum-score-of-a-split.c
+++ 3788-maximum-score-of-a-split/3788-maximum-score-of-a-split.c
@@ -1,21 +1,25 @@
 long long maximumScore(int* nums, int numsSize) {
-    int* suffixMin = malloc(sizeof(int) * numsSize);
     
-    suffixMin[numsSize - 2] = nums[numsSize - 1];
-    for(int i = numsSize - 3; i >= 0; i--){
-        suffixMin[i] = (nums[i+1] < suffixMin[i+1])?nums[i+1] :  suffixMin[i+1];
+    long long prefixSum = 0;
+    for(int i = 0; i < numsSize-1; i++){
+        prefixSum += nums[i];
     }
-    
-    long long prefixSum = nums[0];
-    long long maxScore = prefixSum - suffixMin[0];
 
-    for(int i = 0; i< numsSize-1; i++){
-        long long score = prefixSum - suffixMin[i];
-        prefixSum += nums[i+1];
+    int suffixMin = nums[numsSize -1];
+    
+    long long maxScore = prefixSum - suffixMin;
+
+    for(int i = numsSize-2; i >= 0; i--){
+        
+        long long score = prefixSum - suffixMin;
+        
+        prefixSum -= nums[i];
+        suffixMin = (suffixMin < nums[i])? suffixMin: nums[i];
+        
         if(score > maxScore){
             maxScore = score;
         }
     }
-    free(suffixMin);
+
     return maxScore;
 }
