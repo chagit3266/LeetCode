98bf5f1dca5c88cb64c94dcf0646c8268477caaf
chagit levi <chagit3266@gmail.com>
2026-03-12 19:58:34 +0200

    Time: 0 ms (100%), Space: 14.6 MB (47.83%) - LeetHub

--- 0089-gray-code/0089-gray-code.cpp
+++ 0089-gray-code/0089-gray-code.cpp
@@ -4,10 +4,23 @@ public:
         return n ^ (n >> 1);
     }
     vector<int> grayCode(int n) {
+        // vector<int> nums;
+        // int total = 1 << n;
+        // for(int i = 0; i < total; i++){
+        //     nums.push_back(nextGrayCode(i));
+        // }
+        // return nums;
+
         vector<int> nums;
-        int total = 1 << n;
-        for(int i = 0; i < total; i++){
-            nums.push_back(nextGrayCode(i));
+        if(n < 1)
+           return nums;
+
+        nums.push_back(0);
+
+        for(int i = 0; i < n; i++){
+            for(int j = nums.size()-1; j >= 0 ; j--){
+                nums.push_back(nums[j] | (1 << i));
+            }
         }
         return nums;
     }
